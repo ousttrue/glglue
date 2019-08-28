f7402236145bd5fa8071b18f0e885d6e7d46f68a
ousttrue <ousttrue@gmail.com>
2019-08-29 04:29:12 +0900

    setup.cfg

--- setup.py
+++ setup.py
@@ -1,26 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-from setuptools import setup, find_packages
-import sys
-import glglue
-
-setup(
-    name='glglue',
-    version=glglue.__version__,
-    description='The glue code which mediates between OpenGL and some GUI',
-    long_description=open('README.md', encoding='utf-8').read(),
-    long_description_content_type='text/markdown',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Multimedia :: Graphics :: 3D Modeling',
-    ],
-    keywords=['opengl'],
-    author='ousttrue',
-    author_email='ousttrue@gmail.com',
-    license='zlib',
-    packages=find_packages(),
-    test_suite='nose.collector',
-    tests_require=['Nose'],
-)
+from setuptools import setup
+# setup.cfg
+setup()
