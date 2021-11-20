153b2445f625dbacab050284a94d1fe7bc2e59a2
ousttrue <ousttrue@gmail.com>
2021-11-20 20:20:42 +0900

    3.2 core profile

--- src/glglue/pyside6.py
+++ src/glglue/pyside6.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from PySide6 import QtCore
 from PySide6 import QtOpenGLWidgets
+from PySide6.QtGui import QSurfaceFormat
 from .basecontroller import BaseController
 
 
@@ -9,11 +10,21 @@ class Widget(QtOpenGLWidgets.QOpenGLWidget):
     https://doc.qt.io/qtforpython/PySide6/QtOpenGLWidgets/QOpenGLWidget.html
     '''
 
-    def __init__(self, parent, controller: BaseController, dpi_scale=1.0):
+    def __init__(self, parent, controller: BaseController, dpi_scale=1.0, core_profile=True):
         super().__init__(parent)
         self.controller = controller
         self.setMouseTracking(True)
         self.dpi_Scale = dpi_scale
+
+        if core_profile:
+            format = QSurfaceFormat()
+            format.setDepthBufferSize(24)
+            format.setStencilBufferSize(8)
+            format.setVersion(3, 2)
+            format.setProfile(QSurfaceFormat.CoreProfile)
+            # self.setFormat(format)
+            QSurfaceFormat.setDefaultFormat(format)
+            # must be called before the widget or its parent window gets shown
 
     def minimumSizeHint(self):
         return QtCore.QSize(50, 50)
