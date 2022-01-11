ba5ad3b3a63dbff3ee9aa064af551aeead4303c8
ousttrue <ousttrue@gmail.com>
2022-01-11 20:14:46 +0900

    numpy

--- tests/test_numpy.py
+++ tests/test_numpy.py
@@ -44,6 +44,16 @@ class TestNumpy(unittest.TestCase):
         apply = c @ numpy.array([0, 0, 0, 1])
         self.assertTrue((numpy.array([2, 4, 6, 1]) == apply).all())
 
+    def test_type(self):
+        a = numpy.array([
+            [1, 0, 0, 1],
+            [0, 1, 0, 2],
+            [0, 0, 1, 3],
+            [0, 0, 0, 1],
+        ])
+        print(a, type(a))
+        self.assertIsInstance(a, numpy.ndarray)
+
 
 if __name__ == '__main__':
     unittest.main()
