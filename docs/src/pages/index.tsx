3c975b34a6a11089aff79bdadab8af1dff6d66ca
ousttrue <ousttrue@gmail.com>
2024-05-07 18:25:17 +0900

    fix link

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -8,7 +8,7 @@ import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -29,14 +29,17 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <Link
+          className="button button--secondary button--lg"
+          to="/docs/">
+          glglue
+        </Link>
       </main>
     </Layout>
   );
